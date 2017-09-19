9fe5c7c04e0d03e5686bb9ef3328702bbec32aef
damindaeshan <damindaeshan@gmail.com>
2017-09-19 10:51:44 +1000

    check_ticket_INUse

--- src/bcccp/test/SeasonTicket_Test.java
+++ src/bcccp/test/SeasonTicket_Test.java
@@ -6,10 +6,7 @@ public class SeasonTicket_Test {
 	
 	public static void main(String[] args) {
 		SeasonTicket ticket = new SeasonTicket("TIK001", "LEFD-001", 1220227200, 0220227200);
-		System.out.println(ticket.getId());
-        System.out.println(ticket.getCarparkId());
-        System.out.println(ticket.getStartValidPeriod());
-        System.out.println(ticket.getEndValidPeriod());
+		System.out.println(ticket.inUse());
 
 	}
 	
