607d654a81c89e44fc652547aa826539c3e92035
damindaeshan <damindaeshan@gmail.com>
2017-09-19 10:47:26 +1000

    check_getEndValidPeriod

--- src/bcccp/test/SeasonTicket_Test.java
+++ src/bcccp/test/SeasonTicket_Test.java
@@ -8,6 +8,8 @@ public class SeasonTicket_Test {
 		SeasonTicket ticket = new SeasonTicket("TIK001", "LEFD-001", 1220227200, 0220227200);
 		System.out.println(ticket.getId());
         System.out.println(ticket.getCarparkId());
+        System.out.println(ticket.getStartValidPeriod());
+        System.out.println(ticket.getEndValidPeriod());
 
 	}
 	
