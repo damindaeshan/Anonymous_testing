c9f4d03c1f6102f320491f934d1fc264bcb02f70
damindaeshan <damindaeshan@gmail.com>
2017-09-19 10:59:03 +1000

    Check_currentUsageFucntion

--- src/bcccp/test/SeasonTicket_Test.java
+++ src/bcccp/test/SeasonTicket_Test.java
@@ -6,7 +6,11 @@ public class SeasonTicket_Test {
 	
 	public static void main(String[] args) {
 		SeasonTicket ticket = new SeasonTicket("TIK001", "LEFD-001", 1220227200, 0220227200);
-		System.out.println(ticket.inUse());
+		UsageRecord record = new UsageRecord("TIK001",  1220227200);
+		ticket.recordUsage(record);
+		
+		System.out.println(ticket.getCurrentUsageRecord());
+		//Usage : startDateTime : 1220227200, endDateTime: 0
 
 	}
 	
