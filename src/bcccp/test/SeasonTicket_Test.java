8bbe2d350a1728825a254d055b534ee5e345d70c
damindaeshan <damindaeshan@gmail.com>
2017-09-19 10:43:46 +1000

    check_getcarparkId

--- src/bcccp/test/SeasonTicket_Test.java
+++ src/bcccp/test/SeasonTicket_Test.java
@@ -7,6 +7,7 @@ public class SeasonTicket_Test {
 	public static void main(String[] args) {
 		SeasonTicket ticket = new SeasonTicket("TIK001", "LEFD-001", 1220227200, 0220227200);
 		System.out.println(ticket.getId());
+        System.out.println(ticket.getCarparkId());
 
 	}
 	
