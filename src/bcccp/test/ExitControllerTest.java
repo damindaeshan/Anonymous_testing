35d72ecfab338171df3b1b1bd01ae7fa05295439
gulani09 <gulani09@gmail.com>
2017-09-20 15:45:50 +1000

    Test log(), ExitController.java
    Test log(), ExitController.java
    Output Message:
    ExitController : Welcome for testing

--- src/bcccp/test/ExitControllerTest.java
+++ src/bcccp/test/ExitControllerTest.java
@@ -1,5 +1,33 @@
 package bcccp.test;
 
+import bcccp.carpark.CarSensor;
+import bcccp.carpark.Carpark;
+import bcccp.carpark.Gate;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.exit.ExitController;
+import bcccp.carpark.exit.ExitUI;
+import bcccp.tickets.adhoc.AdhocTicketDAO;
+import bcccp.tickets.adhoc.AdhocTicketFactory;
+import bcccp.tickets.season.SeasonTicketDAO;
+import bcccp.tickets.season.UsageRecordFactory;
+
 public class ExitControllerTest {
+	public static void main (String[] args) {
+		AdhocTicketFactory adhocTicketFactory = new AdhocTicketFactory();
+		AdhocTicketDAO adhocTicketDAO = new AdhocTicketDAO(adhocTicketFactory);
+		UsageRecordFactory factory = new UsageRecordFactory();
+		SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
+
+		Carpark carpark = new Carpark("Gulani_CAR", 15, adhocTicketDAO, seasonTicketDAO);
+		Gate entryGate = new Gate(1, 2);
+		Gate exitGate = new Gate(2, 3);
+		CarSensor is = new CarSensor("CAR1", 1, 2);
+		CarSensor os = new CarSensor("CAR1", 2, 3);
+		EntryUI ui = new EntryUI(1, 2);
+		ExitUI exitUI = new ExitUI(2, 3);
+
+		ExitController exitController = new ExitController(carpark, exitGate, is, os, exitUI);
+		exitController.log("Welcome for testing");
+	}
 
 }
