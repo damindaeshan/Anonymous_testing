1b4a437aa9cd7ac98d53d4a6887e2dad46f9fdb2
Eshan Daminda <damindaeshan@gmail.com>
2017-09-21 09:10:40 +1000

    Get number of tickets

--- src/bcccp/test/SeasonTicketDAO_Test.java
+++ src/bcccp/test/SeasonTicketDAO_Test.java
@@ -10,10 +10,9 @@ public class SeasonTicketDAO_Test {
 		
 		UsageRecordFactory factory = new UsageRecordFactory();
 		
-		
 		SeasonTicketDAO SeaDAO = new SeasonTicketDAO(factory);
 		SeasonTicket ticket = new SeasonTicket("TIK001", "CAR_001", 12343344, 1234565);
-		SeaDAO.deregisterTicket(ticket);
-		//Ticket Deregistered
+		System.out.println(SeaDAO.getNumberOfTickets()); 
+		//1
 	}
 }
