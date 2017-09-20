4e13f0b625aaf4792a385cf493f5eec44e093797
damindaeshan <damindaeshan@gmail.com>
2017-09-21 08:08:16 +1000

    Register ticket

--- src/bcccp/test/SeasonTicketDAO_Test.java
+++ src/bcccp/test/SeasonTicketDAO_Test.java
@@ -1,5 +1,11 @@
 package bcccp.test;
 
 public class SeasonTicketDAO_Test {
+        UsageRecordFactory factory = new UsageRecordFactory();
+		
+		SeasonTicketDAO SeaDAO = new SeasonTicketDAO(factory);
+		SeasonTicket ticket = new SeasonTicket("TIK001", "CAR_001", 12343344, 1234565);
+		SeaDAO.registerTicket(ticket);
+		//Ticket Registered
 
 }
