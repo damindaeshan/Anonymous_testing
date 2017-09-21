f281ff44a0495c469c5f70ece97cd69ba11d3a0b
gulani09 <gulani09@gmail.com>
2017-09-21 16:11:56 +1000

    Test carEventDetected() EXITING block
    Test carEventDetected() EXITING block in ExitControllerTest.java
    Output:
    ExitController : carEventDetected: Exit Car 2, car Detected: true

--- src/bcccp/carpark/exit/ExitController.java
+++ src/bcccp/carpark/exit/ExitController.java
@@ -60,7 +60,7 @@ public class ExitController
 	@Override
 	public void carEventDetected(String detectorId, boolean carDetected) {
 
-		state = STATE.TAKEN;
+		state = STATE.EXITING;
 		log("carEventDetected: " + detectorId + ", car Detected: " + carDetected );
 		
 		switch (state) {
