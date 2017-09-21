6ccf099123176c1f0307fb6ce2224bd5876184b2
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-09-22 09:47:21 +1000

    Override the IAdhocTicketFactory

--- src/bcccp/test/AdhocTicketDAO_TestClass.java
+++ src/bcccp/test/AdhocTicketDAO_TestClass.java
@@ -2,5 +2,12 @@ package bcccp.test;
 
 
 public class AdhocTicketDAO_TestClass  {
-
+//Override the IAdhocTicketFactory
+        IAdhocTicketFactory adhocTicketFactory = new IAdhocTicketFactory() {
+            @Override
+            public IAdhocTicket make(String carparkId, int ticketNo) {
+                String barcode = "A" + Integer.toHexString(ticketNo);
+		return new AdhocTicket(carparkId, ticketNo, barcode);
+            }
+        };
 }
