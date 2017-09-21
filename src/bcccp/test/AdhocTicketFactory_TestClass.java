c5871804d6d8c60ff15c224667a2e0b11289b420
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-09-22 09:52:13 +1000

    Assign Carpark ID

--- src/bcccp/test/AdhocTicketFactory_TestClass.java
+++ src/bcccp/test/AdhocTicketFactory_TestClass.java
@@ -4,5 +4,8 @@ public class AdhocTicketFactory_TestClass {
 public static void main(String[] args) {
 	//Create AdhocTicketFactory object
         AdhocTicketFactory adhocTicketFactory = new AdhocTicketFactory();
+
+//Assign Carpark ID
+        String carparkId = "Level1_Car002";
 }
 }
