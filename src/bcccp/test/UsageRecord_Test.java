57d8d076782981cf3578ddc273972404446a2c8f
Eshan Daminda <damindaeshan@gmail.com>
2017-09-21 15:44:06 +1000

    Check toString method in usage record

--- src/bcccp/test/UsageRecord_Test.java
+++ src/bcccp/test/UsageRecord_Test.java
@@ -4,6 +4,7 @@ package bcccp.test;
   
   UsageRecord usaRec = new UsageRecord("Tick001", 12345677);
   
-     System.out.println(usaRec.getSeasonTicketId());
+     System.out.println(usaRec.toString());
+     //Usage : startDateTime : 12345677 , endDateTime: 0
  
  }
