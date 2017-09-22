8a95af5883e142f761604739646173c1510b8bba
gulani09 <30450425+gulani09@users.noreply.github.com>
2017-09-22 15:11:44 +1000

    Check setStatus() FULL
    Check setStatus() FULL
    in EntryController.java
    input: FULL
    output: EntryController : setState: FULL

--- src/bcccp/carpark/entry/EntryController.java
+++ src/bcccp/carpark/entry/EntryController.java
@@ -138,8 +138,8 @@ public class EntryController
 	public void setState(STATE newState) {
 		//newState = STATE.IDLE;
 		//newState = STATE.BLOCKED;
-		newState = STATE.WAITING;
-		//newState = STATE.FULL;
+		//newState = STATE.WAITING;
+		newState = STATE.FULL;
 		//newState = STATE.VALIDATED;
 		//newState = STATE.ISSUED;
 		//newState = STATE.TAKEN;
