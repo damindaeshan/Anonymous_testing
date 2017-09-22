1c2cc81b39519d6cef95c0151456d1a2e6b4b588
Pradeep Lakshitha <jpradeeplakshitha@gmail.com>
2017-09-22 11:31:26 +1000

    Integration Test 5 : AdhocTicketDAO
    Input 2: Level1_Car003
    Expected Result : CarparkID test is Failed
    Actual Result : CarparkID test is Failed

--- src/bcccp/test/AdhocTicketDAO_TestClass.java
+++ src/bcccp/test/AdhocTicketDAO_TestClass.java
@@ -28,16 +28,20 @@ public class AdhocTicketDAO_TestClass  {
 
 //Check carpark id
 /*
-Integration Test 4 : AdhocTicketDAO
-
 Test Carpark ID
 
 Input 1: Level1_Car002
 Expected Result : CarparkID test is Passed
 Actual Result : CarparkID test is Passed
 
+Input 2: Level1_Car003
+Expected Result : CarparkID test is Failed
+Actual Result : CarparkID test is Failed
+
+
+
 */
-        if(iAdhocTicket.getCarparkId().equals("Level1_Car002")){
+        if(iAdhocTicket.getCarparkId().equals("Level1_Car003")){
             System.out.println("CarparkID test is Passed");
         }else{
             System.out.println("CarparkID test is Failed");
