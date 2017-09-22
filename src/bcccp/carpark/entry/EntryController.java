ec083467c90fd23211a960d5adf84911b9dd9726
gulani09 <30450425+gulani09@users.noreply.github.com>
2017-09-22 15:14:24 +1000

    Check setStatus() ENTERING
    Check setStatus() ENTERING
    in EntryController.java
    input: ENTERING
    output: EntryController : setState: ENTERING

--- src/bcccp/carpark/entry/EntryController.java
+++ src/bcccp/carpark/entry/EntryController.java
@@ -142,8 +142,8 @@ public class EntryController
 		//newState = STATE.FULL;
 		//newState = STATE.VALIDATED;
 		//newState = STATE.ISSUED;
-		newState = STATE.TAKEN;
-		//newState = STATE.ENTERING;
+		//newState = STATE.TAKEN;
+		newState = STATE.ENTERING;
 		//newState = STATE.ENTERED;
 		switch (newState) {
 		
